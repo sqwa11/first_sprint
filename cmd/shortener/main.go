db7d4715285158874c5877390c6df4a2c84aa309
Данияр Ермагамбетов <karioko767.se87@gmail.com>
2024-07-06 22:52:17 +0500

    f

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,73 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strings"
+)
+
+var (
+	urlMap             = make(map[string]string) // Хранение сокращенных URL
+	baseURL            = "http://localhost:8080" // Базовый URL вашего сервиса
+	shortenedURLLength = 8                       // Длина сокращенного URL
+)
+
+func main() {
+
+	http.HandleFunc("/p", handleShorten)
+	http.HandleFunc("/{id}", handleRedirect)
+
+	fmt.Println("Server listening on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleShorten(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Сука", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body := make([]byte, r.ContentLength)
+	_, err := r.Body.Read(body)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	longURL := strings.TrimSpace(string(body))
+	shortURL := generateShortURL()
+
+	urlMap[shortURL] = longURL
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s/%s\n", baseURL, shortURL)
+}
+
+func handleRedirect(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Сука", http.StatusMethodNotAllowed)
+		return
+	}
+
+	id := strings.TrimPrefix(r.URL.Path, "/")
+	longURL, exists := urlMap[id]
+	if !exists {
+		http.Error(w, "Ссылки нет", http.StatusNotFound)
+		return
+	}
+
+	http.Redirect(w, r, longURL, http.StatusTemporaryRedirect)
+}
+
+func generateShortURL() string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" // Доступные символы для генерации
+
+	b := make([]byte, 8)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sqwa11/first_sprint
+
+go 1.22.0
