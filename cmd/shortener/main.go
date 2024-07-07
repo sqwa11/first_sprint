38fc14596530376c889d99e13bb0b47d419c4445
Данияр Ермагамбетов <karioko767.se87@gmail.com>
2024-07-07 21:18:39 +0500

    a1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/sqwa11/first_sprint/internal/app/get"
 	"github.com/sqwa11/first_sprint/internal/app/post"
 	"log"
@@ -12,6 +11,5 @@ func main() {
 	http.HandleFunc("/", post.HandleShorten)
 	http.HandleFunc("/{id}", get.HandleRedirect)
 
-	fmt.Println("Server listening on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
