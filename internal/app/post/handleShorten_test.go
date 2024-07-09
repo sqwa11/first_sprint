e56570d9fc864869d60e57430118071bb78a3834
Данияр Ермагамбетов <karioko767.se87@gmail.com>
2024-07-09 08:26:12 +0500

    ffd

--- internal/app/post/handleShorten_test.go
+++ internal/app/post/handleShorten_test.go
@@ -10,19 +10,16 @@ import (
 )
 
 func TestHandleShorten(t *testing.T) {
-	// Создаем временный HTTP сервер для тестов
 	ts := httptest.NewServer(http.HandlerFunc(HandleShorten))
 	defer ts.Close()
 
-	// Создаем фиктивную конфигурацию для теста
 	cfg := &config.Config{
 		ServerAddress: ts.URL,
 		BaseURL:       ts.URL,
 	}
 	SetBaseURL(cfg.BaseURL)
 
-	// Запрос к временному серверу
-	longURL := "https://example.com"
+	longURL := "https://google.com"
 	body := strings.NewReader(longURL)
 	resp, err := http.Post(ts.URL, "text/plain", body)
 	if err != nil {
@@ -30,7 +27,6 @@ func TestHandleShorten(t *testing.T) {
 	}
 	defer resp.Body.Close()
 
-	// Проверяем корректность ответа
 	if resp.StatusCode != http.StatusCreated {
 		t.Errorf("handler returned wrong status code: got %v want %v", resp.StatusCode, http.StatusCreated)
 	}
@@ -41,15 +37,12 @@ func TestHandleShorten(t *testing.T) {
 	}
 
 	shortURL := strings.TrimSpace(string(responseBody))
-	// Проверяем, что короткий URL начинается с ожидаемого базового URL
 	if !strings.HasPrefix(shortURL, cfg.BaseURL+"/") {
 		t.Errorf("handler returned unexpected body: got %v", shortURL)
 	}
 
-	// Извлекаем идентификатор из короткого URL
 	id := strings.TrimPrefix(shortURL, cfg.BaseURL+"/")
 
-	// Проверяем, что короткий URL сохранен в URLMap
 	savedURL, exists := URLMap[id]
 	if !exists {
 		t.Errorf("short URL not saved in map")
