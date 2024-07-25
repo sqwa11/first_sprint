c42c0fa4b57908969b5b11c2c468a43b9ce10a82
Данияр Ермагамбетов <karioko767.se87@gmail.com>
2024-07-25 18:13:54 +0500

    llll

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"go.uber.org/zap"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -17,8 +19,12 @@ func main() {
 		log.Fatalf("Invalid configuration: %v", err)
 	}
 
+	logger := NewLogger()
+	defer logger.Sync()
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(WithLogging(logger)) // Добавляем middleware логирования
 
 	post.SetBaseURL(cfg.BaseURL)
 
@@ -28,5 +34,59 @@ func main() {
 	log.Printf("Server listening on address %s...\n", cfg.Address)
 	if err := http.ListenAndServe(cfg.Address, r); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
+	}
+}
+
+type responseData struct {
+	status int
+	size   int
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	responseData *responseData
+}
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode
+}
+
+func NewLogger() *zap.SugaredLogger {
+	logger, _ := zap.NewProduction()
+	return logger.Sugar()
+}
+
+func WithLogging(logger *zap.SugaredLogger) func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+
+			responseData := &responseData{
+				status: 0,
+				size:   0,
+			}
+			lw := loggingResponseWriter{
+				ResponseWriter: w,
+				responseData:   responseData,
+			}
+			h.ServeHTTP(&lw, r)
+
+			duration := time.Since(start)
+
+			logger.Infow("Handled request",
+				"uri", r.RequestURI,
+				"method", r.Method,
+				"status", responseData.status,
+				"duration", duration,
+				"size", responseData.size,
+			)
+		})
 	}
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/sqwa11/first_sprint
 go 1.22.0
 
 require github.com/go-chi/chi/v5 v5.1.0
+
+require (
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+)
