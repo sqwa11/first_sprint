b1c3be8572475146c3608b576b4e97a8ba5bb398
Данияр Ермагамбетов <karioko767.se87@gmail.com>
2024-07-29 22:20:05 +0500

    f

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"compress/gzip"
+	"go.uber.org/zap"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/sqwa11/first_sprint/internal/app/config"
 	"github.com/sqwa11/first_sprint/internal/app/get"
 	"github.com/sqwa11/first_sprint/internal/app/post"
-	"io"
-	"log"
-	"net/http"
-	"strings"
 )
 
 func main() {
@@ -24,13 +24,11 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Use(WithLogging(logger))
-	r.Use(DecompressMiddleware)
-	r.Use(CompressMiddleware)
+	r.Use(WithLogging(logger)) // Добавляем middleware логирования
 
 	post.SetBaseURL(cfg.BaseURL)
 
-	r.Post("/api/shorten", post.HandleShorten) // Добавляем маршрут для /api/shorten
+	r.Post("/api/shorten", post.HandleShorten)
 	r.Get("/{id}", get.HandleRedirect)
 
 	log.Printf("Server listening on address %s...\n", cfg.Address)
@@ -39,50 +37,56 @@ func main() {
 	}
 }
 
-// Middleware для распаковки запросов с Content-Encoding: gzip
-func DecompressMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Content-Encoding") == "gzip" {
-			gz, err := gzip.NewReader(r.Body)
-			if err != nil {
-				http.Error(w, "Failed to decompress request body", http.StatusInternalServerError)
-				return
-			}
-			defer gz.Close()
-			r.Body = gz
-		}
-		next.ServeHTTP(w, r)
-	})
+type responseData struct {
+	status int
+	size   int
 }
 
-// Middleware для сжатия ответов с Accept-Encoding: gzip
-func CompressMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		acceptEncoding := r.Header.Get("Accept-Encoding")
-		if acceptEncoding != "" && containsGzip(acceptEncoding) {
-			gzw := gzip.NewWriter(w)
-			defer gzw.Close()
-			w.Header().Set("Content-Encoding", "gzip")
-			w = gzipResponseWriter{Writer: gzw, ResponseWriter: w}
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-type gzipResponseWriter struct {
-	io.Writer
+type loggingResponseWriter struct {
 	http.ResponseWriter
+	responseData *responseData
 }
 
-func (w gzipResponseWriter) Write(b []byte) (int, error) {
-	return w.Writer.Write(b)
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size
+	return size, err
 }
 
-func containsGzip(acceptEncoding string) bool {
-	for _, encoding := range []string{"gzip", "x-gzip"} {
-		if strings.Contains(acceptEncoding, encoding) {
-			return true
-		}
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode
+}
+
+func NewLogger() *zap.SugaredLogger {
+	logger, _ := zap.NewProduction()
+	return logger.Sugar()
+}
+
+func WithLogging(logger *zap.SugaredLogger) func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+
+			responseData := &responseData{
+				status: 0,
+				size:   0,
+			}
+			lw := loggingResponseWriter{
+				ResponseWriter: w,
+				responseData:   responseData,
+			}
+			h.ServeHTTP(&lw, r)
+
+			duration := time.Since(start)
+
+			logger.Infow("Handled request",
+				"uri", r.RequestURI,
+				"method", r.Method,
+				"status", responseData.status,
+				"duration", duration,
+				"size", responseData.size,
+			)
+		})
 	}
-	return false
 }
