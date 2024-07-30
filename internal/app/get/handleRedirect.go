d782e5a9963ff1994eea56a118a1288d195294a7
Данияр Ермагамбетов <karioko767.se87@gmail.com>
2024-07-30 09:39:27 +0500

    f

--- internal/app/get/handleRedirect.go
+++ internal/app/get/handleRedirect.go
@@ -2,15 +2,15 @@ package get
 
 import (
 	"net/http"
-	"strings"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/sqwa11/first_sprint/internal/app/post"
 )
 
 func HandleRedirect(w http.ResponseWriter, r *http.Request) {
-	id := strings.TrimPrefix(r.URL.Path, "/")
-	longURL, exists := post.URLMap[id]
-	if !exists {
+	id := chi.URLParam(r, "id")
+	longURL, ok := post.URLMap[id]
+	if !ok {
 		http.Error(w, "Not Found", http.StatusNotFound)
 		return
 	}
